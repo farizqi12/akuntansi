1b42f97e72de0440faf989a6601d0e4f47019b96
farizqi12 <muhamadfarizqi39@gmail.com>
2025-11-14 12:45:57 +0700

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="bg-amber-300 max-w-md flex items-center justify-center">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium aut ipsum nisi accusamus officia
+            maiores atque, voluptatum perspiciatis! Saepe inventore ipsam ducimus neque nam itaque et suscipit odio
+            cupiditate animi?</p>
+    </div>
+</body>
+
+</html>
